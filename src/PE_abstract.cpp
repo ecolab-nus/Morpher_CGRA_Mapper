213ca10acb4c91dad7216799c4f4f7e1ee1bb934
DhananjayaWijerathne <dmdwijerathne@gmail.com>
2021-09-23 16:11:40 +0800

    Adding PE_abstract class

---
+++ src/PE_abstract.cpp
@@ -0,0 +1,23 @@
+
+#include "FU.h"
+#include "RegFile.h"
+#include "CGRA.h"
+#include <assert.h>
+#include <sstream>
+#include <iostream>
+#include "CGRA.h"
+#include "PE.h"
+#include "Port.h"
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <fstream>
+#include <nlohmann/json.hpp>
+#include <iomanip>
+#include <regex>
+
+namespace CGRAXMLCompile
+{
+
+
+} /* namespace CGRAXMLCompile */
---
+++ src/PE_abstract.h
@@ -0,0 +1,45 @@
+/*
+ * PE_abstract.h
+ *
+ *  Created on: 26 Feb 2018
+ *      Author: manupa
+ */
+
+#ifndef PE_abstract_H_
+#define PE_abstract_H_
+
+#include "Module.h"
+#include "RegFile.h"
+#include <assert.h>
+#include <unordered_set>
+
+namespace CGRAXMLCompile
+{
+
+class PE_abstract
+{
+public:
+	PE_abstract(int x, int y)
+	{
+
+
+		assert(y != -1);
+		assert(x != -1);
+		this->X = x;
+		this->Y = y;
+
+	};
+
+	std::set<PE_abstract *> neighbors;
+
+	int X;
+	int Y;
+
+private:
+
+
+};
+
+} /* namespace CGRAXMLCompile */
+
+#endif /* PE_abstract_H_ */
